295240b0378381d4bdb889d706c924b4fa4180c4
Sean Wejebe <we.sean94@gmail.com>
2016-12-10 13:24:36 -0800

    It begins!

---
+++ Assets/__Scripts/ZineFormatter.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ZineFormatter : MonoBehaviour
+{
+	[SerializeField] protected List<Sprite> _pages;
+}
